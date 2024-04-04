345d4ba9ca30828a83931ca772d7c543fafdaef8
Petr Plsek <me@kalanys.com>
2024-04-04 22:14:43 +0200

    kw_images - access factory

---
+++ php-tests/AccessTests/FactoryTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace AccessTests;
+
+
+use CommonTestClass;
+use kalanis\kw_files\Access\Factory;
+use kalanis\kw_files\FilesException;
+use kalanis\kw_images\Access;
+use kalanis\kw_images\Content;
+use kalanis\kw_images\ImagesException;
+use kalanis\kw_paths\PathsException;
+use kalanis\kw_storage\Storage;
+
+
+class FactoryTest extends CommonTestClass
+{
+    /**
+     * @throws FilesException
+     * @throws PathsException
+     */
+    public function testOperations(): void
+    {
+        $data = $this->getLib()->getOperations();
+        $this->assertNotEmpty($data);
+        $this->assertInstanceOf(Content\BasicOperations::class, $data);
+    }
+
+    /**
+     * @throws FilesException
+     * @throws ImagesException
+     * @throws PathsException
+     */
+    public function testDirs(): void
+    {
+        $data = $this->getLib()->getDirs();
+        $this->assertNotEmpty($data);
+        $this->assertInstanceOf(Content\Dirs::class, $data);
+    }
+
+    /**
+     * @throws FilesException
+     * @throws ImagesException
+     * @throws PathsException
+     */
+    public function testImages(): void
+    {
+        $data = $this->getLib()->getImages();
+        $this->assertNotEmpty($data);
+        $this->assertInstanceOf(Content\Images::class, $data);
+    }
+
+    /**
+     * @throws FilesException
+     * @throws ImagesException
+     * @throws PathsException
+     */
+    public function testUpload(): void
+    {
+        $data = $this->getLib()->getUpload();
+        $this->assertNotEmpty($data);
+        $this->assertInstanceOf(Content\ImageUpload::class, $data);
+    }
+
+    /**
+     * @throws FilesException
+     * @throws PathsException
+     * @return Access\Factory
+     */
+    protected function getLib(): Access\Factory
+    {
+        return new Access\Factory((new Factory())->getClass(
+            new Storage\Storage(new Storage\Key\DefaultKey(), new Storage\Target\Memory())
+        ));
+    }
+}
